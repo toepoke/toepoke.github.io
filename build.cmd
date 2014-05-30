9b0da575b2d1ede4d23355719ef5c4e2514b7ef5
toepoke <github@junge.co.uk>
2014-05-30 12:31:55 +0100

    Updated build scripts

--- build.cmd
+++ build.cmd
@@ -4,7 +4,7 @@ if exist S:\Dropbox set DRIVE=S:
 if not exist S:\Dropbox set DRIVE=C:
 
 %DRIVE%
-cd %DRIVE%\Dropbox\Git\repo\franzjunge.com
+cd %DRIVE%\Dropbox\Git\repo\toepoke.github.io
 
 REM Encoding fix - see http://stackoverflow.com/questions/13146420/my-jekyll-site-cant-build-liquid-exception-incompatible-character-encodings
 chcp 65001
